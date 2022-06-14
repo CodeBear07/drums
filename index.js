b5a84845025cb9a8f94c61c3b6a6e1f4e4839a1e
CodeBear07 <100164455+CodeBear07@users.noreply.github.com>
2022-06-14 19:18:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ document.addEventListener("keypress", function(event) {
 });
 
 function makeSound(key) {
-  var crash = new Audio("sounds/balbebek.ogg")
-  var kickBass = new Audio("sounds/burnin.ogg")
-  var snare = new Audio("sounds/habibi.ogg")
-  var tom1 = new Audio("sounds/kalbim.ogg")
-  var tom2 = new Audio("sounds/kızım.ogg")
-  var tom3 = new Audio("sounds/ily.ogg")
-  var tom4 = new Audio("sounds/lightmy.ogg")
+  var crash = new Audio("sounds/crash.mp3")
+  var kickBass = new Audio("sounds/kick-bass.mp3")
+  var snare = new Audio("sounds/snare.mp3")
+  var tom1 = new Audio("sounds/tom-1.mp3")
+  var tom2 = new Audio("sounds/tom-2.mp3")
+  var tom3 = new Audio("sounds/tom-3.mp3")
+  var tom4 = new Audio("sounds/tom-4.mp3")
 
   switch (key) {
     case "w":
